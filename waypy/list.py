77c3625e25b4619806fe67531f3ef38983d8e001
SACRIER <caravigneh@outloo.com>
2022-03-23 08:54:08 -0300

    Translating the file to English

--- waypy/list.py
+++ waypy/list.py
@@ -1,110 +1,110 @@
-class No(object):
-    def __init__(self, pai=None, valor1=None, valor2=None, anterior=None, proximo=None):
-        self.pai       = pai
-        self.valor1    = valor1
-        self.valor2    = valor2
-        self.anterior  = anterior
-        self.proximo   = proximo
+class Node(object):
+    def __init__(self, father=None, value1=None, value2=None, previous=None, next=None):
+        self.father    = father
+        self.value1    = value1
+        self.value2    = value2
+        self.previous  = previous
+        self.next   = next
 
 class lista(object):
     head = None
     tail = None
 
-    # INSERE NO INÍCIO DA LISTA
-    def inserePrimeiro(self, v1, v2, p):
-        novo_no = No(p, v1, v2, None, None)
+    # INSERT AT THE BEGINNING OF THE LIST
+    def insertFirst(self, v1, v2, p):
+        new_node = Node(p, v1, v2, None, None)
         if self.head == None:
-            self.tail = novo_no
-            self.head = novo_no
+            self.tail = new_node
+            self.head = new_node
         else:
-            novo_no.proximo = self.head
-            self.head.anterior = novo_no
-            self.head = novo_no
+            new_node.next = self.head
+            self.head.previous = new_node
+            self.head = new_node
 
-    # INSERE NO FIM DA LISTA
-    def insereUltimo(self, v1, v2, p):
+    # INSERT AT THE END OF THE LIST
+    def insertLast(self, v1, v2, p):
 
-        novo_no = No(p, v1, v2, None, None)
+        new_node = Node(p, v1, v2, None, None)
 
         if self.head is None:
-            self.head = novo_no
+            self.head = new_node
         else:
-            self.tail.proximo = novo_no
-            novo_no.anterior   = self.tail
-        self.tail = novo_no
+            self.tail.next = new_node
+            new_node.previous   = self.tail
+        self.tail = new_node
 
-    # REMOVE NO INÍCIO DA LISTA
-    def deletaPrimeiro(self):
+    # REMOVE AT THE BEGINNING OF THE LIST
+    def deleteFirst(self):
         if self.head is None:
             return None
         else:
             no = self.head
-            self.head = self.head.proximo
+            self.head = self.head.next
             if self.head is not None:
-                self.head.anterior = None
+                self.head.previous = None
             else:
                 self.tail = None
-            return no
+            return node
 
-    # REMOVE NO FIM DA LISTA
-    def deletaUltimo(self):
+    # REMOVE AT THE END OF THE LIST
+    def deleteLast(self):
         if self.tail is None:
             return None
         else:
-            no = self.tail
-            self.tail = self.tail.anterior
+            node = self.tail
+            self.tail = self.tail.previous
             if self.tail is not None:
-                self.tail.proximo = None
+                self.tail.next = None
             else:
                 self.head = None
-            return no
+            return node
 
-    def primeiro(self):
+    def first(self):
         return self.head
     
-    def ultimo(self):
+    def last(self):
         return self.tail
 
-    def vazio(self):
+    def empty(self):
         if self.head is None:
             return True
         else:
             return False
         
-    def exibeLista(self):
+    def showList(self):
         
         aux = self.head
         str = []
         while aux != None:
             temp = []
-            temp.append(aux.valor1)
-            temp.append(aux.valor2)
+            temp.append(aux.value1)
+            temp.append(aux.value2)
             str.append(temp)
-            aux = aux.proximo
+            aux = aux.next
         
         return str
     
-    def exibeCaminho(self):
+    def showway(self):
         
-        atual = self.tail
-        caminho = []
-        while atual.pai is not None:
-            caminho.append(atual.valor1)
-            atual = atual.pai
-        caminho.append(atual.valor1)
-        caminho = caminho[::-1]
-        return caminho
+        current = self.tail
+        way = []
+        while current.father is not None:
+            way.append(current.value1)
+            current = current.father
+        way.append(current.value1)
+        way = way[::-1]
+        return way
     
-    def exibeCaminho1(self,valor):
+    def showway1(self,value):
                 
-        atual = self.head
-        while atual.valor1 != valor:
-            atual = atual.proximo
+        current = self.head
+        while current.value1 != value:
+            current = current.next
     
-        caminho = []
-        atual = atual.pai
-        while atual.pai is not None:
-            caminho.append(atual.valor1)
-            atual = atual.pai
-        caminho.append(atual.valor1)
-        return caminho
+        way = []
+        current = current.father
+        while current.father is not None:
+            way.append(current.value1)
+            current = current.father
+        way.append(current.value1)
+        return way
